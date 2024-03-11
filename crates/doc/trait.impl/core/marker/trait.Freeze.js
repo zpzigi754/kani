636de3ae392226ae146ffac40e58cd10bc397d74
zpzigi754 <95561719+zpzigi754@users.noreply.github.com>
2024-03-11 07:22:42 +0000

    Deploying to gh-pages from @ zpzigi754/kani@ea710b36c7ef81b038df4c1a9ef3c18d2f6ab0ef 🚀

---
+++ crates/doc/trait.impl/core/marker/trait.Freeze.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"kani":[["impl Freeze for <a class=\"enum\" href=\"kani/futures/enum.SchedulingAssumption.html\" title=\"enum kani::futures::SchedulingAssumption\">SchedulingAssumption</a>",1,["kani::futures::SchedulingAssumption"]],["impl Freeze for <a class=\"struct\" href=\"kani/futures/struct.JoinHandle.html\" title=\"struct kani::futures::JoinHandle\">JoinHandle</a>",1,["kani::futures::JoinHandle"]],["impl Freeze for <a class=\"struct\" href=\"kani/futures/struct.RoundRobin.html\" title=\"struct kani::futures::RoundRobin\">RoundRobin</a>",1,["kani::futures::RoundRobin"]]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
